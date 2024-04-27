8c6d257ae2a018206e7f99d216bcc9d56a00b1eb
batahualpa11 <brianatahualpa@gmail.com>
2024-04-26 20:18:23 -0400

    added pseudocode

--- Main.java
+++ Main.java
@@ -16,16 +16,16 @@ public class Person {
 public class Main {
   public static void main(String[] args) {
     Person myObj = new Person();
-    myObj.name = "John";  // error // we get an error here because the variable name is 
-    System.out.println(myObj.name); // error 
+    myObj.name = "John";  // error // we get an error here because the variable name is declared as private 
+    System.out.println(myObj.name); // error // we also get an error here because the name is still declared as private 
   }
 }
 
 public class Main {
   public static void main(String[] args) {
     Person myObj = new Person();
-    myObj.setName("John"); // Set the value of the name variable to "John"
-    System.out.println(myObj.getName());
+    myObj.setName("John"); // Set the value of the name variable to "John" // here we don't get any errors because it uses the public set method even though it is still declared private  
+    System.out.println(myObj.getName()); // here we 
   }
 }
   
